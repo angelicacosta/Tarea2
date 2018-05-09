3e066f3b1f720aa269827d9e824cba27991ea64e
Giulianne17 <gvtr96@gmail.com>
2018-05-09 17:22:34 -0400

    Caso de prueba: testFinAnio

--- CI3715-Tarea2/Calculo/TestCalculo.py
+++ CI3715-Tarea2/Calculo/TestCalculo.py
@@ -3,3 +3,25 @@ Created on 9 may. 2018
 
 @author: Giulianne Tavano y Angelica Acosta
 '''
+
+import unittest
+from datetime import date, time
+from Calculo.CalculoPrecio import Tarifa, Servicio, calcularPrecio
+
+class TestCalculo(unittest.TestCase):
+    
+    #Caso: inicia a finales de un año y finaliza en principios del otro.
+    def testFinAnio(self):
+        dia1 = date(2018, 12, 30)
+        dia2 = date(2019, 1, 2)
+        horaIni = time(hour=1, minute=20, second=0, microsecond=0)
+        horaFin = time(hour=1, minute=20, second=0, microsecond=0)
+        tarifa = Tarifa(1,2)
+
+        tiempoDeServicio = Servicio(dia1, horaIni, dia2, horaFin)
+        
+
+        assert calcularPrecio(tarifa, tiempoDeServicio)==96, "No se esta calculando bien el precio"
+    
+  
+ 
