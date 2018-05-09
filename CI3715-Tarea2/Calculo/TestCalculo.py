449ebf5ff8f8cde50bca0c32743dd17bbe7f0148
angelicacosta <20612929+angelicacosta@users.noreply.github.com>
2018-05-09 19:17:45 -0400

    Arreglando identacion

--- CI3715-Tarea2/Calculo/TestCalculo.py
+++ CI3715-Tarea2/Calculo/TestCalculo.py
@@ -59,54 +59,54 @@ class TestCalculo(unittest.TestCase):
 		assert calcularPrecio(tarifa, tiempoDeServicio)==1, "No se esta calculando bien el precio"
 
     #Caso: Tiene 7 dias y 1 minuto.
-    def testMas7Dias(self):
-        dia1 = date(2018, 5, 18)
-        dia2 = date(2018, 5, 30)
-        horaIni = time(hour=1, minute=20, second=0, microsecond=0)
-        horaFin = time(hour=1, minute=21, second=0, microsecond=0)
-        tarifa = Tarifa(1,2)
+	def testMas7Dias(self):
+		dia1 = date(2018, 5, 18)
+		dia2 = date(2018, 5, 30)
+		horaIni = time(hour=1, minute=20, second=0, microsecond=0)
+		horaFin = time(hour=1, minute=21, second=0, microsecond=0)
+		tarifa = Tarifa(1,2)
+
+		tiempoDeServicio = Servicio(dia1, horaIni, dia2, horaFin)
         
-        tiempoDeServicio = Servicio(dia1, horaIni, dia2, horaFin)
-        
-        with self.assertRaises(SystemExit) as cm:
-            calcularPrecio(tarifa, tiempoDeServicio)
-        self.assertEqual(cm.exception.code, None)  
+		with self.assertRaises(SystemExit) as cm:
+			calcularPrecio(tarifa, tiempoDeServicio)
+		self.assertEqual(cm.exception.code, None)  
     
     #Caso: Tiene exactamente 7 dias.    
-    def testExact7Dias(self):
-        dia1 = date(2018, 5, 1)
-        dia2 = date(2018, 5, 8)
-        horaIni = time(hour=1, minute=0, second=0, microsecond=0)
-        horaFin = time(hour=1, minute=0, second=0, microsecond=0)
-        tarifa = Tarifa(1,1)
-        
-        tiempoDeServicio = Servicio(dia1, horaIni, dia2, horaFin)
-               
-        assert calcularPrecio(tarifa, tiempoDeServicio)==168, "No se esta calculando bien el precio"
+	def testExact7Dias(self):
+		dia1 = date(2018, 5, 1)
+		dia2 = date(2018, 5, 8)
+		horaIni = time(hour=1, minute=0, second=0, microsecond=0)
+		horaFin = time(hour=1, minute=0, second=0, microsecond=0)
+		tarifa = Tarifa(1,1)
+
+		tiempoDeServicio = Servicio(dia1, horaIni, dia2, horaFin)
+    
+		assert calcularPrecio(tarifa, tiempoDeServicio)==168, "No se esta calculando bien el precio"
 	
 	#Caso: Tiene menos de 7 dias.
-    def testMenos7Dias(self):
-        dia1 = date(2018, 5, 18)
-        dia2 = date(2018, 5, 20)
-        horaIni = time(hour=1, minute=20, second=0, microsecond=0)
-        horaFin = time(hour=1, minute=30, second=0, microsecond=0)
-        tarifa = Tarifa(1,1)
-        
-        tiempoDeServicio = Servicio(dia1, horaIni, dia2, horaFin)
-        
-        assert calcularPrecio(tarifa, tiempoDeServicio)==49, "No se esta calculando bien el precio"
+	def testMenos7Dias(self):
+		dia1 = date(2018, 5, 18)
+		dia2 = date(2018, 5, 20)
+		horaIni = time(hour=1, minute=20, second=0, microsecond=0)
+		horaFin = time(hour=1, minute=30, second=0, microsecond=0)
+		tarifa = Tarifa(1,1)
+
+		tiempoDeServicio = Servicio(dia1, horaIni, dia2, horaFin)
+
+		assert calcularPrecio(tarifa, tiempoDeServicio)==49, "No se esta calculando bien el precio"
 
 	#Caso: Acepta bisiestos.  
-    def testAnioBisiesto(self):
-        dia1 = date(2020, 2, 28)
-        dia2 = date(2020, 3, 1)
-        horaIni = time(hour=3, minute=0, second=0, microsecond=0)
-        horaFin = time(hour=3, minute=0, second=0, microsecond=0)
-        tarifa = Tarifa(1,1)
-       
-        tiempoDeServicio = Servicio(dia1, horaIni, dia2, horaFin)
-               
-        assert calcularPrecio(tarifa, tiempoDeServicio)==48, "No se esta calculando bien el precio"
-    
+	def testAnioBisiesto(self):
+		dia1 = date(2020, 2, 28)
+		dia2 = date(2020, 3, 1)
+		horaIni = time(hour=3, minute=0, second=0, microsecond=0)
+		horaFin = time(hour=3, minute=0, second=0, microsecond=0)
+		tarifa = Tarifa(1,1)
+
+		tiempoDeServicio = Servicio(dia1, horaIni, dia2, horaFin)
+
+		assert calcularPrecio(tarifa, tiempoDeServicio)==48, "No se esta calculando bien el precio"
+
 if __name__ == "__main__":  
-    unittest.main()        
+	unittest.main()        
